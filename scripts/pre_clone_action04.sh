d201f8f207fc40a28562113ce03bc3e900bb35bf
Marywanmary <70870062+Marywanmary@users.noreply.github.com>
2025-08-31 22:38:22 +0800

    Add files via upload
    Signed-off-by: Marywanmary <70870062+Marywanmary@users.noreply.github.com>

---
+++ scripts/pre_clone_action04.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASE_PATH=$(cd $(dirname $0)/.. && pwd)
+
+Dev=$1
+
+CONFIG_FILE="$BASE_PATH/deconfig/$Dev.config"
+INI_FILE="$BASE_PATH/compilecfg/$Dev.ini"
+
+if [[ ! -f $CONFIG_FILE ]]; then
+    echo "Config not found: $CONFIG_FILE"
+    exit 1
+fi
+
+if [[ ! -f $INI_FILE ]]; then
+    echo "INI file not found: $INI_FILE"
+    exit 1
+fi
+
+read_ini_by_key() {
+    local key=$1
+    awk -F"=" -v key="$key" '$1 == key {print $2}' "$INI_FILE"
+}
+
+REPO_URL=$(read_ini_by_key "REPO_URL")
+REPO_BRANCH=$(read_ini_by_key "REPO_BRANCH")
+REPO_BRANCH=${REPO_BRANCH:-main}
+
+# 提取芯片系列（用于缓存共享）
+CHIPSET=$(echo "$Dev" | cut -d'_' -f1)
+BUILD_DIR="$BASE_PATH/action_build"
+
+echo $REPO_URL $REPO_BRANCH
+echo "$REPO_URL/$REPO_BRANCH" >"$BASE_PATH/repo_flag"
+echo "$CHIPSET" >"$BASE_PATH/chipset_flag"
+
+# 如果构建目录已存在，先删除它
+if [[ -d $BUILD_DIR ]]; then
+    rm -rf $BUILD_DIR
+fi
+
+git clone --depth 1 -b $REPO_BRANCH $REPO_URL $BUILD_DIR
+
+# GitHub Action 移除国内下载源
+PROJECT_MIRRORS_FILE="$BUILD_DIR/scripts/projectsmirrors.json"
+
+if [ -f "$PROJECT_MIRRORS_FILE" ]; then
+    sed -i '/.cn\//d; /tencent/d; /aliyun/d' "$PROJECT_MIRRORS_FILE"
+fi
